f72e71abf47a497b690774f1234d8162b7e6a75a
Chris Kennedy <c@groovy.org>
2024-02-10 06:34:13 -0800

    allow adding more args on script

--- scripts/system_health.sh
+++ scripts/system_health.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
+
 target/release/rsllm \
     --daemon \
     --query "Determine if the system is healthy or sick, diagnose the issue if possible or give details about it. Use the historical view to see bigger trends of the system." \
     --system-prompt "you are able to say green or red depending on th system health determined from system stats analysis." \
-    --ai-os-stats
+    --ai-os-stats \
+    $@
