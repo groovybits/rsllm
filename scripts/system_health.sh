744347b916aefbb3ce0c3bbf0d47a9f24a0efe21
Chris Kennedy <c@groovy.org>
2024-02-10 08:07:47 -0800

    add more verbose system stats prompts with charts
    metrics views

--- scripts/system_health.sh
+++ scripts/system_health.sh
@@ -3,7 +3,7 @@
 
 target/release/rsllm \
     --daemon \
-    --query "Determine if the system is healthy or sick, diagnose the issue if possible or give details about it. Use the historical view to see bigger trends of the system." \
-    --system-prompt "you are able to say green or red depending on th system health determined from system stats analysis." \
+    --query "Determine if the system is healthy or sick, diagnose the issue if possible or give details about it. Use the historical view to see bigger trends of the system. draw a table of the current system metrics vs. historical showing changes over time." \
+    --system-prompt "you are able to say green or red depending on the system health determined from system stats analysis. you can draw tables of system metrics." \
     --ai-os-stats \
     $@
