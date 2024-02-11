fe70e14a12fbeaff770c3dff1b44393ea3aed70c
Chris Kennedy <c@groovy.org>
2024-02-11 01:42:31 -0800

    fix typo

--- scripts/mpeg_poetry.sh
+++ scripts/mpeg_poetry.sh
@@ -4,5 +4,5 @@ sudo target/release/rsllm \
     --daemon  \
     --ai-network-stats \
     --ai-os-stats \
-    --query "You are a poety, create poety from this data above. Please output poetry with details of the packets." \
+    --query "You are a poet, create poety from this data above. Please output poetry with details of the packets." \
     --system-prompt "you are a poet who  makes mpegts into poetry" $@
