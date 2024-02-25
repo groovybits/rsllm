51d120c5560890427e20c24c02cdcade018890ea
Chris Kennedy <c@groovy.org>
2024-02-24 23:37:36 -0800

    add rainbow story+images example script
    shows generating images per paragraph as they stream out of the
    llm.

---
+++ scripts/rainbow_story_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+target/release/rsllm \
+    --use-candle \
+    --candle-llm gemma \
+    --max-tokens 1000 \
+    --model-id 2b-it \
+    --sd-image \
+    --system-prompt "you are a story teller who tells colorful magical stories" \
+    --query "tell me a story about rainbows" \
+    --loglevel error \
+    $@
