fe27bb047ab7c790b21a19775d62a2a340f12954
Chris Kennedy <c@groovy.org>
2024-03-06 00:15:07 -0800

    improve mpeg analyzer
    has issues, need to hook up the mpeg2ts/h264 reader crates since
    the LLMs do not decode hex much at all and make things up :D.
    
    someday we will have LLMs tuned on video packet specs.

--- scripts/mpeg_analyzer.sh
+++ scripts/mpeg_analyzer.sh
@@ -2,9 +2,12 @@
 #
 sudo DYLD_LIBRARY_PATH=`pwd`:/usr/local/lib:$DYLD_LIBRARY_PATH target/release/rsllm \
     --daemon  \
-    --ai-network-stats \
+    --system-prompt "You are an expert  MpegTS Analyzer that can decode and decipher hex packets and general statistics of MpegTS. You report the status and health of the stream, alerting when anything is wrong. Do not make up stats, only use what you can verifiably see in the context." \
+    --query "Analyze the timeline shown in the historical context of mpeg packets information and if present the raw hexdumps too. Give a report of NAL information and general errors or any bad timing, IAT issues, or other tr101290 type errors. Look for captions and scte35 packets and report on them, and any other SEI messages you see in the packets. Output in an interface looking set of values summarized from the data relevant to the current issues you see in the stream. Do not make up information, only report based off of the data in the context history." \
+    --use-api \
     --pcap-stats \
-    --system-prompt "You are an expert  MpegTS Analyzer that can decode and decipher hex packets and general statistics of MpegTS. You report the status and health of the stream, alerting when anything is wrong." \
-    --query "Analyze the following timeline of mpeg packets information and raw hexdumps. Give a report of NAL information and general errors or any bad timing, IAT issues, or other tr101290 type errors. Look for captions and scte35 packets and report on them, and any other SEI messages you see in the packets. Output in an interface looking set of values summarized from the data relevant to the current issues you see in the stream. If everything is alright, generate a nice image description of some nature scene." \
+    --ai-network-stats \
     --ai-network-hexdump $@
+
+## Needs a lot of LLM context, works best with llama.cpp API mode with Mixtral LLM
 
