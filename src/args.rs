1daf6a6cb6f95e9c9c119483b7b510a924060d0b
Chris Kennedy <c@groovy.org>
2024-03-17 16:53:43 -0700

    v0.4.6 - metal exclusion w/out mps feature

--- src/args.rs
+++ src/args.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.4.5",
+    version = "0.4.6",
     about = "Rust LLM Stream Analyzer and Content Generator"
 )]
 pub struct Args {
