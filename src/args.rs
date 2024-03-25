9bfd6f2cb72900f856d42cdccd4a94d2c1551e48
Chris Kennedy <c@groovy.org>
2024-03-24 21:40:39 -0700

    v0.5.12 - image gen config and stability

--- src/args.rs
+++ src/args.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 #[clap(
     author = "Chris Kennedy",
-    version = "0.5.11",
+    version = "0.5.12",
     about = "Rust AI Stream Analyzer Twitch Bot"
 )]
 pub struct Args {
