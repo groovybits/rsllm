fa5d739b18123da4622a3d62bc9cf01916ff7023
Chris Kennedy <c@groovy.org>
2024-03-27 00:05:12 -0700

    use all 32k context for mixtral llama.cpp daemon

--- bin/llama_start.sh
+++ bin/llama_start.sh
@@ -6,7 +6,7 @@ MODEL=/Volumes/BrahmaSSD/LLM/models/GGUF/dolphin-2.7-mixtral-8x7b.Q8_0.gguf
 server \
     -m $MODEL \
     -c 0 \
-    -np 2 \
+    -np 1\
     --port 8080 \
     -ngl 60 \
     -t 24 \
