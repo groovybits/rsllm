79fca90843b2893929ed6ecd5a0d414883fe56b7
Chris Kennedy <c@groovy.org>
2024-03-26 17:36:32 -0700

    add ./bin/ with startup scripts for API servers

---
+++ bin/llama_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#MODEL=/Volumes/BrahmaSSD/LLM/models/GGUF/dolphin-2.7-mixtral-8x7b.Q5_K_M.gguf
+MODEL=/Volumes/BrahmaSSD/LLM/models/GGUF/dolphin-2.7-mixtral-8x7b.Q8_0.gguf
+
+server \
+    -m $MODEL \
+    -c 0 \
+    -np 2 \
+    --port 8080 \
+    -ngl 60 \
+    -t 24 \
+    --host 0.0.0.0 $@
---
+++ bin/start_sdwebui.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+#
+./webui.sh --api --api-log --nowebui --port 7860 --skip-torch-cuda-test --no-half --use-cpu all
+#./webui.sh --api --api-log --port 7860 --skip-torch-cuda-test --no-half --use-cpu all
